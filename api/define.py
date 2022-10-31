25e62e5083a68e3409beeb00ca51caac70a17c7a
alaa-adoos <om0331549@gmail.com>
2022-10-31 23:09:18 +0300

    define

--- api/define.py
+++ api/define.py
@@ -2,31 +2,44 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
+
+
 class handler(BaseHTTPRequestHandler):
 
+  def do_GET(self):
+    '''
+        you can send response to vercel as a country or a capital
+        and send request to API to get a response of the capital of the country
+        or the country to this capital  and if you didint sent any 
+        it will give you a message said provide a country or a capital
+    '''
 
-    def do_GET(self):
-        s=self.path
-        url_components=parse.urlsplit(s)
-        query_string_list=parse.parse_qsl(url_components.query)
-        dictionary=dict(query_string_list)
-        definsions=[]
-        if 'word' in dictionary:
-            word=dictionary['word']
-            url='https://api.dictionaryapi.dev/api/v2/entries/en'
-            r=requests.get(url+word)
-            data=r.json
+    path=self.path
+    url_Component=parse.urlsplit(path)
+    query_string=parse.parse_qsl(url_Component.query)
+    dictionary=dict(query_string)
+    
+    if "country" in dictionary:
+        country=dictionary["country"]
+        api_url="https://restcountries.com/v3.1/name/"
+        r=requests.get(api_url + country)
+        data=r.json()
+        capital=str(data[0]["capital"][0])
+        message = f'the capital of {country} is {capital} '    
 
-            for word_data in data:
-                definsion=word_data['meanings'][0]['definitions'][0]['definitions']
-                definsions(definsion)
-            message=str(definsions)
-        else:
-            message="please enter a words"
+    elif "capital" in dictionary:
+        capital=dictionary["capital"]
+        api_url="https://restcountries.com/v3.1/capital/"
+        r=requests.get(api_url+capital)
+        data=r.json()
+        country=str(data[0]["name"]["common"])
+        message = f'{capital} is the capital of {country} '    
+    else :
+        message="please provide me with a country or a capital"
+     
 
-
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write(message.encode())
-        return
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(message.encode())
+    return
