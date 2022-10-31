48d7a59bbb4674946c85a318662b5c263a718fa3
alaa-adoos <om0331549@gmail.com>
2022-10-31 19:57:23 +0300

    fit:add welcoming funcion2

--- api/hoppy.py
+++ api/hoppy.py
@@ -2,7 +2,8 @@ from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
 
-    def do_Get(self):
+
+    def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
